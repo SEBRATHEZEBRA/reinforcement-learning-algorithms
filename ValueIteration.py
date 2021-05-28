305310d6f2a7d4ea28158b0a4065d252138f32aa
Sebastian Oliver <sebtheoliver@gmail.com>
2021-05-28 17:35:41 +0200

    Implemented a checkFree function to ensure the randomly generated coordinates aren't already in use.

--- ValueIteration.py
+++ ValueIteration.py
@@ -2,24 +2,41 @@ from sys import argv
 from random import randint
 
 states = []
-width = 0
-height = 0
+width = int(argv[1])
+height = int(argv[2])
 
 # k is the number of landmines
 landmines = []
-k = 9
+k = 3
 
 start = [0, 0]
 end = [0, 0]
 
 gamma = 0.9
 
+# Directions the robot can move.
 directions = {
     "UP":[0, 1],
     "DOWN":[0, -1],
     "LEFT":[-1, 0],
     "RIGHT":[1, 0]
 }
+
+# Returns a random x value in the gridworld.
+def getRandomX():
+    return randint(0, width - 1)
+
+# Returns a random y value in the gridworld.
+def getRandomY():
+    return randint(0, height - 1)
+
+# Checks if the coordinates [x, y] are being used as the start state, end state or landmines.
+def checkFree(x, y):
+
+    if [x, y] in landmines or [x, y] == start or [x, y] == end:
+        return False
+
+    return True
 
 
 def main():
@@ -28,34 +45,33 @@ def main():
         print("Correct use: python3 Reinforcement.py <width> <height> [options]")
         return
 
-    width = int(argv[1])
-    height = int(argv[2])
-
     if (len(argv) == 3):
-        start[0] = randint(0, width - 1)
-        start[1] = randint(0, height - 1)
+        start[0] = getRandomX()
+        start[1] = getRandomY()
 
-        end[0] = randint(0, width - 1)
-        end[1] = randint(0, height - 1)
+        x = getRandomX()
+        y = getRandomY()
 
-        if start[0] == end[0]:
-            end[0] = randint(0, width - 1)
-        elif start[1] == end[1]:
-            end[1] = randint(0, height - 1)
+        while not checkFree(x, y):
+            x = getRandomX()
+            y = getRandomY()
+
+        end[0] = x
+        end[1] = y
 
         for i in range(k):
-            landmines.append([randint(0, width - 1), randint(0, height - 1)])
-            if start[0] == landmines[i][0] or end[0] == landmines[i][0]:
-                landmines[i][0] = randint(0, width - 1)
-            elif start[1] == landmines[i][1] or end[1] == landmines[i][1]:
-                landmines[i][1] = randint(0, height - 1)
+
+            x = getRandomX()
+            y = getRandomY()
+
+            while not checkFree(x, y):
+                x = getRandomX()
+                y = getRandomY()
+
+            landmines.append([x, y])
 
     print(start, "", end)
     print(landmines)
-
-    if [1, 2] in landmines:
-        print("[1, 2] is a landmine.")
-
 
 
 if __name__ == "__main__":
