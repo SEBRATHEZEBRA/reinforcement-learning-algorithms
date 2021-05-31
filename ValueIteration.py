a7416beaddbd5b4c0f0a832c4909f26b54f0b986
Sebastian Oliver <sebtheoliver@gmail.com>
2021-05-31 19:26:21 +0200

    Implemented options when used on the command line.

--- ValueIteration.py
+++ ValueIteration.py
@@ -50,6 +50,37 @@ def checkFree(x, y):
 
     return True
 
+# Generates a random position for the start state.
+def genRandomStart():
+    start[0] = getRandomY()
+    start[1] = getRandomX()
+
+# Generates a random position for the end state.
+def genRandomEnd():
+    x = getRandomX()
+    y = getRandomY()
+
+    while not checkFree(x, y):
+        x = getRandomX()
+        y = getRandomY()
+
+    end[0] = y
+    end[1] = x
+
+# Generates random positions for the landmines.
+def genRandomMines():
+
+    for i in range(k):
+
+        x = getRandomX()
+        y = getRandomY()
+
+        while not checkFree(x, y):
+            x = getRandomX()
+            y = getRandomY()
+
+        landmines.append([y, x])
+
 # Returns a list of legal actions a robot can perform from a set of coordinates (x, y) in the grid.
 def getLegalActions(x, y):
 
@@ -168,30 +199,44 @@ def startVI():
 
 def main():
 
-    if (len(argv) == 3):
-        start[0] = getRandomY()
-        start[1] = getRandomX()
+    # Checking if any options are selected.
+    if len(argv) == 3:
+        genRandomStart()
+        genRandomEnd()
+        genRandomMines()
 
-        x = getRandomX()
-        y = getRandomY()
+    # Triggering the correct actions when ceratin options are selected.
+    elif len(argv) > 3:
 
-        while not checkFree(x, y):
-            x = getRandomX()
-            y = getRandomY()
+        if "-start" not in argv:
+            genRandomStart()
 
-        end[0] = y
-        end[1] = x
+        if "-end" not in argv:
+            genRandomEnd()
 
-        for i in range(k):
-
-            x = getRandomX()
-            y = getRandomY()
-
-            while not checkFree(x, y):
-                x = getRandomX()
-                y = getRandomY()
-
-            landmines.append([y, x])
+        for i in range(3, len(argv)):
+            if argv[i] == "-start":
+                start[0] = argv[i + 2]
+                start[1] = argv[i + 1]
+                i += 2
+            elif argv[i] == "-end":
+                end[0] = argv[i + 2]
+                end[1] = argv[i + 1]
+                i += 2
+            elif argv[i] == "-k":
+                k = argv[i + 1]
+                i += 1
+            elif argv[i] == "-gamma":
+                gamma = argv[i + 1]
+                i += 1
+            else:
+                print("Incorrect option.")
+                print("Correct options are:")
+                print("-start xpos ypos")
+                print("-end xpos ypos")
+                print("-k num")
+                print("-gamma g")
+                return
 
     startVI()
     getOptPol()
